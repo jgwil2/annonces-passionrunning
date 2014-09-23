c507a07d7fb1256f97f2461ddd398cc4eede43c8
jgwil2 <jgwil2@gmail.com>
2014-09-23 13:25:12 +0200

    remove unused filter

--- src/Me/PassionBundle/Resources/public/js/secureApp/directives.js
+++ src/Me/PassionBundle/Resources/public/js/secureApp/directives.js
@@ -29,15 +29,3 @@ annoncesApp.directive('closeColorbox', function(){
     }
   };
 });
-
-annoncesApp.filter('regex', function() {
-  return function(input, field, regex) {
-    var patt = new RegExp(regex);      
-    var out = [];
-    for (var i = 0; i < input.length; i++){
-    if(patt.test(input[i][field]))
-      out.push(input[i]);
-    }      
-    return out;
-  };
-});
